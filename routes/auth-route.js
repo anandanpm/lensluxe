639341d69b1f3e6f0a6621102f4d563ae6b03699
anandanpm <ananda1732001@gmail.com>
2025-09-09 12:49:36 +0530

    change done in the google authentication

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -28,21 +28,29 @@ router.get('/google',passport.authenticate("google",{
 //     // res.send(req.user)
 //     res.redirect('/loginedhome')
 // })
-router.get('/google/redirect', passport.authenticate('google', { failureRedirect: '/login' }), async (req, res, next) => {
+router.get(
+  "/google/redirect",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  async (req, res, next) => {
     try {
-     
-      if ( req.user.is_verified === 1) {
-        
-        return res.redirect('/loginedhome');
+      if (!req.user) {
+        console.log("No user found in req.user after Google login");
+        return res.redirect("/login");
+      }
+
+      console.log("Authenticated user:", req.user);
+
+      // Make sure you're checking the right field & type
+      if (req.user.is_verified === 1 || req.user.is_verified === true) {
+        return res.redirect("/loginedhome");
       } else {
-        
-        console.log('User is not verified');
-        return res.redirect('/login');
+        console.log("User is not verified:", req.user);
+        return res.redirect("/login");
       }
     } catch (err) {
-     
       next(err);
     }
-  });
+  }
+);
 
 module.exports = router;
